d1b9695de487fc01acd1f874d4a7f8115515ecc8
Ivan <ivanpetrachenkov.web@gmail.com>
2023-07-20 20:10:12 +0300

    new block

--- js/scripts.js
+++ js/scripts.js
@@ -298,6 +298,65 @@ $(() => {
 	}
 
 
+	//
+	if ($('.articles__slider').length) {
+		new Swiper('.articles__slider', {
+			loop: false,
+			watchSlidesProgress: true,
+			watchOverflow: true,
+			spaceBetween: 15,
+			slidesPerView: 'auto',
+			preloadImages: false,
+			lazy: {
+				loadPrevNext: true,
+				elementClass: 'lazyload',
+				enabled: true,
+				loadedClass: 'loaded',
+				checkInView: true,
+				loadOnTransitionStart: true
+			},
+			navigation: {
+				nextEl: '.slider-button-next',
+				prevEl: '.slider-button-prev'
+			},
+			pagination: {
+				el: ".slider-progressbar",
+				type: "progressbar",
+			},
+			breakpoints: {
+				'320': {
+					slidesPerView: 'auto',
+				},
+				'480': {
+					slidesPerView: 'auto',
+				},
+				'640': {
+					slidesPerView: 'auto',
+				},
+				'1025': {
+					slidesPerView: 3,
+				}
+			},
+			on: {
+				init: function (swiper) {
+					if (swiper.visibleSlides.length < swiper.slides.length) {
+						$(swiper.$el).addClass('_pad')
+					} else {
+						$(swiper.$el).removeClass('_pad')
+					}
+				},
+				resize: function (swiper) {
+					if (swiper.visibleSlides.length < swiper.slides.length) {
+						$(swiper.$el).addClass('_pad')
+					} else {
+						$(swiper.$el).removeClass('_pad')
+					}
+				}
+			}
+		})
+	}
+
+
 	if ($('.service-adv__slider').length) {
 		new Swiper('.service-adv__slider', {
 			loop: false,
@@ -582,70 +641,6 @@ $(() => {
 			offset_top: 64
 		})
 	}
-
-
-	if ($('.product-info__slider').length) {
-		var thumbs = new Swiper(".product-info__thumb", {
-			loop: false,
-			slidesPerView: 4,
-			spaceBetween: 12,
-			watchSlidesProgress: true,
-			watchOverflow: true,
-			preloadImages: false,
-			initialSlide: 1,
-			lazy: {
-				loadPrevNext: true,
-				elementClass: 'lazyload',
-				enabled: true,
-				loadedClass: 'loaded',
-				checkInView: true,
-				loadOnTransitionStart: true
-			},
-			breakpoints: {
-				'320': {
-					slidesPerView: 4,
-					spaceBetween: 12
-				},
-				'480': {
-					slidesPerView: 4,
-					spaceBetween: 12
-				},
-				'768': {
-					slidesPerView: 8,
-					spaceBetween: 20
-				}
-			}
-		})
-
-		new Swiper(".product-info__slider", {
-			loop: false,
-			speed: 500,
-			spaceBetween: 20,
-			watchOverflow: true,
-            watchSlidesProgress: true,
-			slidesPerView: 1,
-			effect: 'fade',
-			initialSlide: 1,
-			fadeEffect: {
-				crossFade: true
-			},
-			lazy: {
-				loadPrevNext: true,
-				elementClass: 'lazyload',
-				enabled: true,
-				loadedClass: 'loaded',
-				checkInView: true,
-				loadOnTransitionStart: true
-			},
-			navigation: {
-				nextEl: '.slider-button-next',
-				prevEl: '.slider-button-prev'
-			},
-			thumbs: {
-				swiper: thumbs
-			}
-		})
-	}
 });
 
 
@@ -677,6 +672,20 @@ $(window).on('load', () => {
 
 
 	$("#dropzone-photo").dropzone({
+		url: "/file/post",
+		addRemoveLinks: true,
+		dictRemoveFile: '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" fill="none" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/><path d="M15 9L9 15" fill="none" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/><path d="M9 9L15 15" fill="none" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>',
+		dictCancelUploadConfirmation : '<svg width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M22.5 11.0799V11.9999C22.4988 14.1563 21.8005 16.2545 20.5093 17.9817C19.2182 19.7088 17.4033 20.9723 15.3354 21.5838C13.2674 22.1952 11.0573 22.1218 9.03447 21.3744C7.01168 20.6271 5.28465 19.246 4.11096 17.4369C2.93727 15.6279 2.37979 13.4879 2.52168 11.3362C2.66356 9.18443 3.49721 7.13619 4.89828 5.49694C6.29935 3.85768 8.19279 2.71525 10.2962 2.24001C12.3996 1.76477 14.6003 1.9822 16.57 2.85986" fill="none" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/><path d="M22.5 4L12.5 14.01L9.5 11.01" fill="none" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>',
+	});
+
+	$("#dropzone-video").dropzone({
+		url: "/file/post",
+		addRemoveLinks: true,
+		dictRemoveFile: '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" fill="none" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/><path d="M15 9L9 15" fill="none" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/><path d="M9 9L15 15" fill="none" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>',
+		dictCancelUploadConfirmation : '<svg width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M22.5 11.0799V11.9999C22.4988 14.1563 21.8005 16.2545 20.5093 17.9817C19.2182 19.7088 17.4033 20.9723 15.3354 21.5838C13.2674 22.1952 11.0573 22.1218 9.03447 21.3744C7.01168 20.6271 5.28465 19.246 4.11096 17.4369C2.93727 15.6279 2.37979 13.4879 2.52168 11.3362C2.66356 9.18443 3.49721 7.13619 4.89828 5.49694C6.29935 3.85768 8.19279 2.71525 10.2962 2.24001C12.3996 1.76477 14.6003 1.9822 16.57 2.85986" fill="none" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/><path d="M22.5 4L12.5 14.01L9.5 11.01" fill="none" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>',
+	});
+
+	$("#dropzone-audio").dropzone({
 		url: "/file/post",
 		addRemoveLinks: true,
 		dictRemoveFile: '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z" fill="none" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/><path d="M15 9L9 15" fill="none" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/><path d="M9 9L15 15" fill="none" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>',
